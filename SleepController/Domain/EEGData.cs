20ef86dfc1f348b3ca9b612eb29dc593d91328d3
Pavel Levchuk <pavel.levchuk@dataart.com>
2019-04-28 17:20:01 +0300

    Update folder structure and add EEG message

---
+++ SleepController/Domain/EEGData.cs
@@ -0,0 +1,15 @@
+﻿namespace SleepController.Domain
+{
+    public readonly struct EEGData
+    {
+        public short Signal01A1 { get; }
+
+        public short Signal0zA2 { get; }
+
+        public short Signal02A2 { get; }
+
+        public short SignalA1A2 { get; }
+
+        public short SignalExt { get; }
+    }
+}
