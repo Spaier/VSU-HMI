e9c669e2da1ea0dd324a20fcdc4a00e9f7feff39
Pavel Levchuk <pavel.levchuk@dataart.com>
2019-04-29 00:11:52 +0300

    Use min and max average threshold

--- SleepController/Domain/ClosedEyesDetector.cs
+++ SleepController/Domain/ClosedEyesDetector.cs
@@ -6,14 +6,16 @@ namespace SleepController.Domain
 {
     public class ClosedEyesDetector
     {
-        public int ClosedEyesThreshold => 30;
+        public int ClosedEyesMinThreshold => 20;
+
+        public int ClosedEyesMaxThreshold => 45;
 
         public bool IsClosed(IEnumerable<EEGEntry> batch)
         {
-            var ebola = batch.Select(it => it.SignalO1A1).ToList();
-            var average = ebola.Sum(it => Math.Abs(it)) / batch.Count();
+            var average = batch.Select(it => it.SignalO1A1)
+                .Sum(it => Math.Abs(it)) / batch.Count();
 
-            return average >= ClosedEyesThreshold;
+            return average >= ClosedEyesMinThreshold && average <= ClosedEyesMaxThreshold;
         }
     }
 }
