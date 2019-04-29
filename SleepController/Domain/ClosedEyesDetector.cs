ed92ba52f1f2b95cbd09c94a3bfa115143f1edf6
Pavel Levchuk <pavel.levchuk@dataart.com>
2019-04-29 12:23:19 +0300

    Compare every value

--- SleepController/Domain/ClosedEyesDetector.cs
+++ SleepController/Domain/ClosedEyesDetector.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SleepController.Domain
 {
     public class ClosedEyesDetector
     {
-        public int ClosedEyesMinThreshold => 10;
-
-        public int ClosedEyesMaxThreshold => 25;
+        public int ClosedEyesMinThreshold => 40;
 
         public int NextWeight => 2;
 
@@ -18,16 +17,16 @@ namespace SleepController.Domain
 
         public bool IsClosed(IEnumerable<EEGEntry> batch, out int average)
         {
-            average = batch.Select(it => it.SignalO1A1)
-                .Sum(it => it) / batch.Count();
+            var signalO1A1 = batch.Select(it => it.SignalO1A1);
+            average = signalO1A1.Sum(it => it) / signalO1A1.Count();
 
             FloatingAverage = (PreviousWeight * FloatingAverage + NextWeight * average)
                 / (NextWeight + PreviousWeight);
 
-            var antialiasing = Math.Abs(FloatingAverage - average);
-
-            return antialiasing >= ClosedEyesMinThreshold
-                && antialiasing <= ClosedEyesMaxThreshold;
+            // var antialiasing = Math.Abs(FloatingAverage - average);
+            return signalO1A1
+                .AsParallel()
+                .Any(it => Math.Abs(it - FloatingAverage) >= ClosedEyesMinThreshold);
         }
     }
 }
